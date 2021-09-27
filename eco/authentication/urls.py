a35acfb71dc4e7dc97d9efe5c5064feccf8db6e5
tower <43769362+languede@users.noreply.github.com>
2021-09-27 21:31:09 +0800

    ready to add authentication model
    bug fixes and going to build authentication model

--- eco/authentication/urls.py
+++ eco/authentication/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import home
+from .views import home, signup, signin
 
 
 urlpatterns = [
     path('', home, name="home"),
-    path('signup', views.signup, name="signup"),
-    path('signin', views.signin, name="signin"),
+    path('signup', signup, name="signup"),
+    path('signin', signin, name="signin"),
 ]
