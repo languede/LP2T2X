0ae55c60d18e9305c1614417febbe55288d2b06a
tower <43769362+languede@users.noreply.github.com>
2021-09-27 19:20:27 +0800

    Revert "covert pure html to dynamic Django templates"
    This reverts commit 93e8af5a3d8999ad7ae876bb6f53c96194e0d7cc.

--- eco/eco/settings.py
+++ eco/eco/settings.py
@@ -121,12 +121,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = BASE_DIR / 'assets'
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    BASE_DIR / 'static/',
-)
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / 'media/'
 MEDIA_URL = '/media/'
 
 # Default primary key field type
