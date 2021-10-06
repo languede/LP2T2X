8a56a0e7aeeb298509b98d7c88f537723b391551
pidepiper009 <huxian126@gmail.com>
2021-10-07 01:15:33 +0800

    修改了一下login功能，完成了logout功能，login后profile页面能够显示用户信息

--- eco/reward/urls.py
+++ eco/reward/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import reward_home_view, login_signup_view, user_profile_view, eco_rating_view
+from . import views
 
 
 urlpatterns = [
-    path('index/', reward_home_view, name="reward_home"),
-    path('login-signup/', login_signup_view, name="login-signup"),
-    path('profile/', user_profile_view, name="profile"),
-    path('rating/', eco_rating_view, name="rating"),
+    path('index/', views.reward_home_view, name="reward_home"),
+    path('login-signup/', views.login_signup_view, name="login-signup"),
+    path('logout/', views.logoutUser, name="logout"),
+    path('profile/', views.user_profile_view, name="profile"),
+    path('rating/', views.eco_rating_view, name="rating"),
 ]
